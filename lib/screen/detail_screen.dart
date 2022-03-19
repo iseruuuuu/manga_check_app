badc919712df4366ad735394a521d6b45f099fe2
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2022-03-19 14:49:33 +0900

    add: create detail

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -7,7 +7,18 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('詳細画面'),
+        title: Text('フェアリーテイル'),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Text('フェアリーテイル'),
+              SizedBox(height: 40),
+              Text('持っている巻数'),
+            ],
+          ),
+        ),
       ),
     );
   }
